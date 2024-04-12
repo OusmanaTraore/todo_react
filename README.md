e2dafc689882440bbc7c5af0d8223f0dc86ec63e
Ousmana Traore <traoreosman@yahoo.fr>
2024-04-13 01:26:27 +0200

    firsts menu (Tracker and Settings)

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # todo_react
+## [1] Create Menu
+    delete all fie inside components folder
+    delete all fie inside pages folder
+    create a new file Menu.tsx
+## [2] Create Menu
+## [3] Create Menu
+## [4] Create Menu
+## [5] Create Menu
+## [6] Create Menu
+## [7] Create Menu
+## [8] Create Menu
+## [9] Create Menu
+## [10] Create Menu
---
+++ todoApp/src/pages/Menu.tsx
@@ -0,0 +1,48 @@
+import { IonContent, IonHeader, IonIcon, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import { settingsOutline, cashOutline } from 'ionicons/icons'; 
+import Tracker from './Tracker';
+import Settings from './Settings';
+import { Redirect, Route } from 'react-router';
+
+const Menu: React.FC = () => {
+    const paths = [
+        {name: 'Tracker', url:'/tracker', icon:cashOutline},
+        {name: 'Settings', url:'/settings', icon:settingsOutline},
+
+    ]
+
+    return (
+        <IonPage>
+       <IonSplitPane contentId="main">
+        <IonMenu contentId="main">
+        <IonHeader>
+                <IonToolbar>
+                    <IonTitle>TASKS</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+                {paths.map((item,index) => (
+                    <IonMenuToggle key={index} autoHide={false}>
+                        <IonItem routerLink={item.url} routerDirection="forward">
+                            <IonIcon icon={item.icon} slot="start"></IonIcon>
+                            {item.name}
+
+                        </IonItem>
+                    </IonMenuToggle>
+                ))}
+            </IonContent>
+        </IonMenu>
+        <IonRouterOutlet id="main">
+        <Route exact path="/tracker" component={Tracker}/>
+        <Route exact path="/settings" component={Settings}/>
+        <Route exact path="/">
+            <Redirect to="/tracker" />
+            </Route>
+        </IonRouterOutlet>
+       </IonSplitPane>
+        </IonPage>
+    );
+};
+
+export default Menu;
